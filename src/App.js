6adbdd06fe793ef6107c2e247a7128b0b6524e80
Gabor Pfeiffer <gaborpfeiffer221@gmail.com>
2021-12-20 18:02:52 +0100

    gh pages routing

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
 						<AppState>
 							<AppNavigation />
 							<Routes>
-								<Route path="winloss_calculator/calculator" element={<CalculatorPage />} />
-								<Route path="winloss_calculator/strategies" element={<StrategiesPage />} />
+								<Route path="#/winloss_calculator/calculator" element={<CalculatorPage />} />
+								<Route path="#/winloss_calculator/strategies" element={<StrategiesPage />} />
 							</Routes>
 						</AppState>
 					</BrowserRouter>
--- src/components/AppNavigation.js
+++ src/components/AppNavigation.js
@@ -5,10 +5,10 @@ const AppNavigation = () => {
 	return (
 		<div className="navigation">
 			<span className="bottom-left-radius">
-				<Link to="winloss_calculator/calculator" >TP/SL calculator</Link>
+				<Link to="#/winloss_calculator/calculator" >TP/SL calculator</Link>
 			</span>
 			<span className="bottom-right-radius">
-				<Link to="winloss_calculator/strategies">Strategies</Link>
+				<Link to="#/winloss_calculator/strategies">Strategies</Link>
 			</span>
 		</div>
 	)
